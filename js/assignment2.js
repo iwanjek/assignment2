ad56b7494fc2c8eb22d1756af4c762a30a760504
ian wanjek <ian.wanjek@gmail.com>
2016-10-06 18:08:23 -0400

    Applied Style Guide to Project

--- js/assignment2.js
+++ js/assignment2.js
@@ -1,32 +1,29 @@
-function subFunc()
-{
-  var registry=document.getElementById("registry");
-  var pass_registry=true;
+(function subFunc(){
+
+  var registry = document.getElementById('registry');
+  var pass_registry = true;
+  var has_allegiance = false;
   var fullname;
   var alias;
   var power;
   var allegiance;
-  var has_allegiance=false;
-  for(var i=1; i<4; i++)
-  {
+
+  for(var i = 1; i < 4; i++){
     var input=registry.elements[i];
-    if(input.value.length==0 || input.value=="Error: Enter a " + input.name)
-    {
-      document.getElementById("registry").elements[i].value="Error: Enter a " + input.name;
-    //  alert("Please Enter a " + input.name);
+
+    if(input.value.length === 0 || input.value === 'Error: Enter a ' + input.name){
+      document.getElementById('registry').elements[i].value = 'Error: Enter a ' + input.name;
       pass_registry=false;
     }
 
   }
-  var fullname=registry.elements[1].value;
-  var alias=registry.elements[2].value;
-  var power=registry.elements[3].value;
+  var fullname = registry.elements[1].value;
+  var alias = registry.elements[2].value;
+  var power = registry.elements[3].value;
 
-  if(pass_registry)
-  {
-    document.getElementById("registry").style.display="none";
-    alert("Congratualtions! You have successfully registered with the Gotham Data Center.")
+  if(pass_registry){
+    document.getElementById('registry').style.display = 'none';
+    alert('Congratualtions! You have successfully registered with the Gotham Data Center.')
   }
 
-
-}
+})();
